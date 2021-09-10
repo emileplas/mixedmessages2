b5d8e477bc0680c3f687e800dd043c969d323a78
emileplas <39679292+emileplas@users.noreply.github.com>
2021-09-10 07:16:55 -0300

    created a While loop to leave program running except negative response from user

--- mixedmessages.js
+++ mixedmessages.js
@@ -9,6 +9,9 @@
 //declaration of promt sync function
 const prompt = require('prompt-sync')({sigint: true});
 
+//declaration of leaveProgram
+var leaveProgram = false;
+
 //declaration of mixed messages array
 var messagesArray = [
     "Raquel and Emile met in 2017.", 
@@ -31,7 +34,7 @@ console.log("\n" +
 "\n Everytime you type YES to the question do you want to know more, you will be given a random messages about Emile and Raquel so you can get to know them more." +
 "\n" +
 "\n"); 
-const input = prompt("Do you want to get to know more about Raquel and Emile?", "answer");
+const input = prompt("Do you want to get to know more about Raquel and Emile? ", "answer");
 
 //create a mixed message
 function mixedmessages(){
@@ -45,13 +48,40 @@ if (input === "YES" || input === "yes" || input === " yes" || input === " YES") 
     console.log("\n");
     mixedmessages();
     console.log("\n");
+    var firstAnswer = 1
   } else {
     console.log(
         "\n" +
         "INVALID INPUT" +
-        '\n To get a fun fact about Raquel and Emile, you need to type YES' +
+        '\n To get a fun fact about Raquel and Emile, you need to type YES.' +
         "\n");
   
   }
 
+while (leaveProgram === false) {
+    if (firstAnswer === 1){
+        console.log("\n");
+        const inputTwo = prompt("Thank you for using the program, do you want to get another fun fact? Type YES for another fun fact. Type NO to leave the program: ", "answer");
+        if(inputTwo === "YES" || inputTwo === "yes" || inputTwo === " yes" || inputTwo === " YES") {
+            console.log("\n");
+            mixedmessages();
+            console.log("\n");
+        }
+        else if (inputTwo === "NO" || inputTwo === "no" || inputTwo === " no" || inputTwo === " NO") {
+            console.log("\n");
+            console.log("Sad to see you go. Have a wonderful day!");
+            console.log("\n");
+            leaveProgram = true;
+        }
+        else{
+            console.log(
+                "\n" +
+                "INVALID INPUT" +
+                '\n To get a fun fact about Raquel and Emile, you need to type YES.' +
+                '\n To leave the program, type NO.' +
+                "\n");
+        }
+
+    }  
+}
 
